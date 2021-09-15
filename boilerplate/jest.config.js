7940a95540792820c1b7eeec6374251dd43645ed
Henrique <henrique.dsvlima@gmail.com>
2021-09-15 14:54:27 -0300

    storybook + features

--- boilerplate/jest.config.js
+++ boilerplate/jest.config.js
@@ -4,5 +4,8 @@ module.exports = {
     collectCoverage: true,
     collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/stories.tsx'],
     setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
-    modulePaths: ['<rootDir>/src/']
+    moduleNameWrapper: {
+        '^styled-components':
+            '<rootDir>/node_modules/styled-components/dist/styled-components.browser.cjs.js'
+    }
 }
